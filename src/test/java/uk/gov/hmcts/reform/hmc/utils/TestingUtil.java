641520269587f0db5f8b51cb2bd521f1bab70fbd
it.consulting@tutanota.com <it.consulting@tutanota.com>
2022-04-05 16:39:24 +0100

    HMAN-145: [PUT Hearing Response] add mandatory validation around field        [HMAN-145] (https://tools.hmcts.net/jira/browse/HMAN-145).

--- src/test/java/uk/gov/hmcts/reform/hmc/utils/TestingUtil.java
+++ src/test/java/uk/gov/hmcts/reform/hmc/utils/TestingUtil.java
@@ -6,6 +6,8 @@ import uk.gov.hmcts.reform.hmc.client.model.hmi.HearingCaseStatus;
 import uk.gov.hmcts.reform.hmc.client.model.hmi.HearingCode;
 import uk.gov.hmcts.reform.hmc.client.model.hmi.HearingDetailsRequest;
 import uk.gov.hmcts.reform.hmc.client.model.hmi.HearingResponse;
+import uk.gov.hmcts.reform.hmc.client.model.hmi.HearingStatus;
+import uk.gov.hmcts.reform.hmc.client.model.hmi.ListingStatus;
 import uk.gov.hmcts.reform.hmc.client.model.hmi.MetaResponse;
 
 import java.time.LocalDateTime;
@@ -51,6 +53,38 @@ public  class TestingUtil {
         hearingCaseStatus.setCode(HearingCode.CLOSED);
         hearingCaseStatus.setDescription("value");
         hearing.setHearingCaseStatus(hearingCaseStatus);
+        hearing.setHearingIdCaseHQ("47743382");
+        hearing.setHearingStartTime(LocalDateTime.now());
+        hearing.setHearingEndTime(LocalDateTime.now());
+        hearing.setHearingCaseIdHmcts("SW710014");
+        hearing.setHearingTranslatorRequired(false);
+        hearing.setHearingCreatedDate(LocalDateTime.now());
+        hearing.setHearingCreatedBy("sysadm");
+        hearingResponse.setHearing(hearing);
+
+        HearingDetailsRequest request = new HearingDetailsRequest();
+        request.setHearingResponse(hearingResponse);
+        return request;
+    }
+
+    public static HearingDetailsRequest getHearingWithCodesRequest() {
+        HearingResponse hearingResponse = new HearingResponse();
+
+        MetaResponse metaResponse = new MetaResponse();
+        metaResponse.setTransactionIdCaseHQ("123");
+        metaResponse.setTimestamp(LocalDateTime.now());
+        hearingResponse.setMeta(metaResponse);
+
+        Hearing hearing = new Hearing();
+        hearing.setHearingCaseVersionId(123);
+        HearingCaseStatus hearingCaseStatus = new HearingCaseStatus();
+        hearingCaseStatus.setCode(HearingCode.CLOSED);
+        hearingCaseStatus.setDescription("value");
+        hearing.setHearingCaseStatus(hearingCaseStatus);
+        HearingStatus hearingStatus = new HearingStatus();
+        hearingStatus.setDescription("vale");
+        hearingStatus.setCode(ListingStatus.DRAFT);
+        hearing.setHearingStatus(hearingStatus);
         hearing.setHearingIdCaseHQ("47743382");
         hearing.setHearingStartTime(LocalDateTime.now());
         hearing.setHearingEndTime(LocalDateTime.now());
