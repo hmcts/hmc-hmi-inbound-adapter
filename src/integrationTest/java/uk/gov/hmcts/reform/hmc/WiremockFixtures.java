bb6baccfd716710ceff32c8fe1556c06217329b5
markdathorne <mark.dathorne@cgi.com>
2022-04-25 12:45:04 +0100

    HMAN-144 Build fix - revert * import.

--- src/integrationTest/java/uk/gov/hmcts/reform/hmc/WiremockFixtures.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/hmc/WiremockFixtures.java
@@ -14,7 +14,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import uk.gov.hmcts.reform.hmc.utils.TestingUtil;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static java.net.HttpURLConnection.HTTP_ACCEPTED;
 import static java.net.HttpURLConnection.HTTP_NOT_FOUND;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
