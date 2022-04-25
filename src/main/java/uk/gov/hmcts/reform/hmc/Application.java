60bcc4c7dee7e14bc69f63c056f5567f7ac36800
markdathorne <mark.dathorne@cgi.com>
2022-04-25 11:50:06 +0100

    HMAN-144 Pipeline build add CVE-2022-22968 suppression

--- src/main/java/uk/gov/hmcts/reform/hmc/Application.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/Application.java
@@ -9,7 +9,7 @@ import uk.gov.hmcts.reform.idam.client.IdamApi;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @EnableFeignClients(basePackageClasses = { IdamApi.class, ServiceAuthorisationApi.class })
-@SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, it's not a utility class
+@SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, it is not a utility class
 public class Application {
 
     public static void main(final String[] args) {
