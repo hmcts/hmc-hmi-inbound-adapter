7670b7fce5a4d9b48b22e23d0110ee8245f0908b
Antony Leons <antony@leons.dev>
2024-10-31 11:39:57 +0000

    fix integration

--- src/main/java/uk/gov/hmcts/reform/hmc/client/model/hmi/HearingStatus.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/client/model/hmi/HearingStatus.java
@@ -1,6 +1,6 @@
 package uk.gov.hmcts.reform.hmc.client.model.hmi;
 
-import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.reform.hmc.exceptions.ValidationError;
@@ -9,6 +9,8 @@ import uk.gov.hmcts.reform.hmc.validator.HearingStatusCodeEnumPattern;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
+
+
 @Data
 @NoArgsConstructor
 public class HearingStatus {
@@ -16,7 +18,7 @@ public class HearingStatus {
     @NotEmpty(message = ValidationError.HEARING_CODE_NULL)
     @Size(max = 30, message = ValidationError.HEARING_CODE_LENGTH)
     @HearingStatusCodeEnumPattern(enumClass = HearingStatusCode.class, fieldName = "hearing status code")
-    @ApiModelProperty(allowableValues = "DRAFT, FIXED, PROV, CNCL")
+    @Schema(allowableValues = "DRAFT, FIXED, PROV, CNCL")
     private String code;
 
     private String description;
