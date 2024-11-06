d36c233c200bd48ef2ef14a1e312ccb6c77a1f54
Antony Leons <antony@leons.dev>
2024-11-06 12:50:14 +0000

    CME-52 (#197)
    * java 21
    
    * Revert "java 21"
    
    This reverts commit 76781e2e36eff221906731e8a49715c0729d524d.
    
    * spring 2.7.x
    
    * fix integration
    
    * update Gradle wrapper to version 8.10.2 and remove unused Jacoco report formats
    
    * java 21
    
    This reverts commit 57eb144e3be83887945ad8c45b85bb2fba1bc9bb.
    
    * outputlocation
    
    * reduce diff
    
    * reduce diff
    
    * Update Dockerfile
    
    * bump gradle

--- src/main/java/uk/gov/hmcts/reform/hmc/client/model/hmi/HearingStatus.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/client/model/hmi/HearingStatus.java
@@ -1,6 +1,6 @@
 package uk.gov.hmcts.reform.hmc.client.model.hmi;
 
-import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.reform.hmc.exceptions.ValidationError;
@@ -16,7 +16,7 @@ public class HearingStatus {
     @NotEmpty(message = ValidationError.HEARING_CODE_NULL)
     @Size(max = 30, message = ValidationError.HEARING_CODE_LENGTH)
     @HearingStatusCodeEnumPattern(enumClass = HearingStatusCode.class, fieldName = "hearing status code")
-    @ApiModelProperty(allowableValues = "DRAFT, FIXED, PROV, CNCL")
+    @Schema(allowableValues = "DRAFT, FIXED, PROV, CNCL")
     private String code;
 
     private String description;
