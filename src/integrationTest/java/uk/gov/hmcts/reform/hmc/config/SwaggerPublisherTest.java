afb6a1f51db7a0dde07f4d42a32a68c9152b9a4a
dinesh1patel <74076102+dinesh1patel@users.noreply.github.com>
2025-09-03 15:43:00 +0100

    Fix Swagger FT: revert class (#213)
    * revert class
    
    * test worklfow api build

--- src/integrationTest/java/uk/gov/hmcts/reform/hmc/config/SwaggerPublisherTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/hmc/config/SwaggerPublisherTest.java
@@ -1,10 +1,8 @@
 package uk.gov.hmcts.reform.hmc.config;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import uk.gov.hmcts.reform.hmc.BaseTest;
@@ -13,7 +11,6 @@ import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -27,48 +24,18 @@ class SwaggerPublisherTest extends BaseTest {
     @Autowired
     private MockMvc mvc;
 
-    private static final String FAILED_TO_LOAD_REMOTE_CONFIG = "Failed to load remote configuration";
-    private static final String UNCAUGHT_SYNTAX_ERROR = "Uncaught SyntaxError";
-    private static final String UNCAUGHT_REFERENCE_ERROR = "Uncaught ReferenceError";
-    private static final String UNCAUGHT_TYPE_ERROR = "Uncaught TypeError";
-
-    @DisplayName("Generate swagger documentation - Successful Test")
-    @Disabled("Skipped for testing pipeline")
+    @DisplayName("Generate swagger documentation")
     @Test
-    void generateDocsSuccess() throws Exception {
-        MockHttpServletResponse response = mvc.perform(get("/v3/api-docs/?test=ggg"))
+    @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
+    void generateDocs() throws Exception {
+        byte[] specs = mvc.perform(get("/v3/api-docs"))
             .andExpect(status().isOk())
             .andReturn()
-            .getResponse();
-
-        String responseContent = new String(response.getContentAsByteArray());
-
-        System.out.println("Response = " + responseContent);
-
-        assertThat(responseContent).doesNotContain(FAILED_TO_LOAD_REMOTE_CONFIG)
-            .doesNotContain(UNCAUGHT_SYNTAX_ERROR)
-            .doesNotContain(UNCAUGHT_REFERENCE_ERROR)
-            .doesNotContain(UNCAUGHT_TYPE_ERROR);
+            .getResponse()
+            .getContentAsByteArray();
 
         try (OutputStream outputStream = Files.newOutputStream(Paths.get("/tmp/swagger-specs.json"))) {
-            outputStream.write(response.getContentAsByteArray());
+            outputStream.write(specs);
         }
-    }
-
-    @DisplayName("Generate swagger documentation - Failing Test")
-    @Disabled("Skipped for testing pipeline")
-    @Test
-    void generateDocsFailure() throws Exception {
-        MockHttpServletResponse response = mvc.perform(get("/v3/api-docs/?test=ggg"))
-            .andExpect(status().isOk())
-            .andReturn()
-            .getResponse();
-        String responseContent = new String(response.getContentAsByteArray())
-            + FAILED_TO_LOAD_REMOTE_CONFIG + UNCAUGHT_REFERENCE_ERROR + UNCAUGHT_TYPE_ERROR + UNCAUGHT_SYNTAX_ERROR;
-
-        assertThat(responseContent).contains("Failed to load remote configuration")
-            .contains(UNCAUGHT_SYNTAX_ERROR)
-            .contains(UNCAUGHT_REFERENCE_ERROR)
-            .contains(UNCAUGHT_TYPE_ERROR);
     }
 }
