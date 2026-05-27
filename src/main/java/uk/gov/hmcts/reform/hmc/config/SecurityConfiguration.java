42746c3d695f8de2a35c872e5fd8a52e472f829d
paulridout <84783658+paulridout@users.noreply.github.com>
2026-05-27 10:50:37 +0100

    HMAN-1089: Requests now require authenticated user (#274)
    * HMAN-1089: Requests now require authenticated user
    
    Changed Spring security filter chain configuration so that HTTP requests are only permitted for authenticated users.
    
    Added oauth2ResourceServer to Spring security filter chain to authenticate users via user token in request.  This required the addition of a JwtDecoder that validated both the timestamp and issuer.
    
    Changed filter order to ensure the service auth filter is evaluated before the BearerTokenAuthenticationFilter, which has been added by the oauth2ResourceServer.
    
    Added properties and environment variables for specifying user token issuer and allowed issuers.
    
    Added base class for integration tests that require Spring security.  Added WireMock configuration to support OAuth2 authentication.  Added class for generating keys for OAuth2 authentication.
    
    Added integrationTestAnnotationProcessor for lombok to build.gradle so that lombok annotations can be used in integration tests.
    
    * Bumping chart version/ fixing aliases
    
    * HMAN-1089: Added IDAM_ODIC_URL to values.yaml
    
    Added entry for IDAM_OIDC_URL to environment section of values.yaml.  The value of it will need to be changed in the production cnp-flux-config file to hmcts-access, just as it is for hmc-cft-hearing-service.
    
    * HMAN-1089: Revert update to version number in Chart.yaml
    
    Reverting unnecessary update to version number in Chart.yaml.  The version number only needs to be incremented once for this set of changes which was already done by pipeline checks.
    
    ---------
    
    Co-authored-by: hmcts-jenkins-d-to-i <62423932+hmcts-jenkins-d-to-i[bot]@users.noreply.github.com>
    Co-authored-by: dinesh1patel <74076102+dinesh1patel@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/hmc/config/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/hmc/config/SecurityConfiguration.java
@@ -1,14 +1,24 @@
 package uk.gov.hmcts.reform.hmc.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.JwtTimestampValidator;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
 import uk.gov.hmcts.reform.authorisation.filters.ServiceAuthFilter;
+import uk.gov.hmcts.reform.hmc.config.validator.MultiIssuerValidator;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -30,11 +40,16 @@ public class SecurityConfiguration {
         "/"
     };
 
+    private final String issuerUri;
+    private final IdamSecurityConfig idamSecurityConfig;
     private final ServiceAuthFilter serviceAuthFilter;
 
     @Autowired
-    public SecurityConfiguration(ServiceAuthFilter serviceAuthFilter) {
-        super();
+    public SecurityConfiguration(@Value("${oidc.issuer-uri}") String issuerUri,
+                                 IdamSecurityConfig idamSecurityConfig,
+                                 ServiceAuthFilter serviceAuthFilter) {
+        this.issuerUri = issuerUri;
+        this.idamSecurityConfig = idamSecurityConfig;
         this.serviceAuthFilter = serviceAuthFilter;
     }
 
@@ -46,13 +61,25 @@ public class SecurityConfiguration {
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-            .addFilterBefore(serviceAuthFilter, AbstractPreAuthenticatedProcessingFilter.class)
-            .authorizeHttpRequests(ar -> ar.anyRequest().permitAll())
+            .addFilterBefore(serviceAuthFilter, BearerTokenAuthenticationFilter.class)
+            .authorizeHttpRequests(ar -> ar.anyRequest().authenticated())
             .sessionManagement(sm -> sm.sessionCreationPolicy(STATELESS))
-            .httpBasic(hb -> hb.disable())
-            .formLogin(form -> form.disable())
-            .logout(logout -> logout.disable())
-            .csrf(csrf -> csrf.disable())
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+            .logout(AbstractHttpConfigurer::disable)
+            .csrf(AbstractHttpConfigurer::disable)
+            .oauth2ResourceServer(oa -> oa.jwt(jwt -> jwt.decoder(jwtDecoder())))
             .build();
+    }
+
+    JwtDecoder jwtDecoder() {
+        OAuth2TokenValidator<Jwt> withTimestamp = new JwtTimestampValidator();
+        OAuth2TokenValidator<Jwt> withMultiIssuer = new MultiIssuerValidator(idamSecurityConfig.getAllowedIssuers());
+        OAuth2TokenValidator<Jwt> validator = new DelegatingOAuth2TokenValidator<>(withTimestamp, withMultiIssuer);
+
+        NimbusJwtDecoder jwtDecoder = JwtDecoders.fromOidcIssuerLocation(issuerUri);
+        jwtDecoder.setJwtValidator(validator);
+
+        return jwtDecoder;
     }
 }
